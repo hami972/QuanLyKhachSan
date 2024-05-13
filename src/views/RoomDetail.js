1ca67feb3889758d10591d5ed6aaf3b4d0ad5cbc
unknown <21522425@gm.uit.edu.vn>
2024-05-13 11:15:14 +0700

    Fix css of profile page

--- src/views/RoomDetail.js
+++ src/views/RoomDetail.js
@@ -9,83 +9,83 @@ import 'react-slideshow-image/dist/styles.css';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 const RoomDetail = ({ rooms }) => {
-    const { roomId } = useParams();
-    const history = useHistory();
-    const room = rooms.find(room => room.id === roomId);
-    const collection = [
-        { src: '/images/nvsgdcaocap.png', alt: "Caption one" },
-        { src: '/images/nvstieuchuan1.png', alt: "Caption two" },
-        { src: '/images/doicaocap2.png', alt: "Caption three" },
-        { src: '/images/doitieuchuan1.png', alt: "Caption four" },
-        { src: '/images/doncaocap1.png', alt: "Caption five" },
-        { src: '/images/doncaocap2.png', alt: "Caption six" },
-      ];
-      const handleGoBack = () => {
-        history.goBack();
-      };
-      if (!room) return <div>Phòng không tồn tại.</div>;
-      return (
-        <div>
-          <TopNav />
-          <section>
-          <div className="row" style={{ backgroundColor: "#905700", color: "#FFF" }}>
-            <div >
-              <button > <img src="/images/GoBack_128px.png" style={{width: '50px'}}  alt="Back"/> </button>
-              <h3 align="center">{room.name}</h3>
-              <p className='px-5' style={{fontSize: '24px'}}>Giá thành: {room.cost}/đêm</p>
-              <p className='px-5' style={{fontSize: '20px'}}>{room.description}</p>
-            </div>
-            <div style={{ backgroundColor: "#fff", padding: '50px', color: '#905700'}}>
-              <div className='form'>
+  const { roomId } = useParams();
+  const history = useHistory();
+  const room = rooms.find(room => room.id === roomId);
+  const collection = [
+    { src: '/images/nvsgdcaocap.png', alt: "Caption one" },
+    { src: '/images/nvstieuchuan1.png', alt: "Caption two" },
+    { src: '/images/doicaocap2.png', alt: "Caption three" },
+    { src: '/images/doitieuchuan1.png', alt: "Caption four" },
+    { src: '/images/doncaocap1.png', alt: "Caption five" },
+    { src: '/images/doncaocap2.png', alt: "Caption six" },
+  ];
+  const handleGoBack = () => {
+    history.goBack();
+  };
+  if (!room) return <div>Phòng không tồn tại.</div>;
+  return (
+    <div>
+      <TopNav />
+      <section>
+        <div className="row g-0" style={{ backgroundColor: "#905700", color: "#FFF" }}>
+          <div >
+            <button > <img src="/images/GoBack_128px.png" style={{ width: '50px' }} alt="Back" /> </button>
+            <h3 align="center">{room.name}</h3>
+            <p className='px-5' style={{ fontSize: '24px' }}>Giá thành: {room.cost}/đêm</p>
+            <p className='px-5' style={{ fontSize: '20px' }}>{room.description}</p>
+          </div>
+          <div style={{ backgroundColor: "#fff", padding: '50px', color: '#905700' }}>
+            <div className='form'>
               <form className='row'>
                 <div className="col-2 mb-3">
-                  <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700'}} type='date' placeholder='Ngày check in'/>
+                  <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700' }} type='date' placeholder='Ngày check in' />
                 </div>
                 <div className="col-2 mb-3">
-                  <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700'}} type='date' placeholder='Ngày check out'/>
+                  <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700' }} type='date' placeholder='Ngày check out' />
                 </div>
                 <div className="col-2 mb-3">
-                  <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700'}} type='number' placeholder='Số lượng người lớn'/>
+                  <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700' }} type='number' placeholder='Số lượng người lớn' />
                 </div>
                 <div className="col-2 mb-3">
-                  <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700'}} type='number' placeholder='Số lượng trẻ em'/>
+                  <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700' }} type='number' placeholder='Số lượng trẻ em' />
                 </div>
                 <div className="col-3 mb-3 ">
-                <NavLink to="/thanhtoan"  className='btn d-flex align-items-center justify-content-center' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700', fontSize: '22px', fontWeight: 'bold', align: 'middle'}}>
-                  Đặt phòng
-                </NavLink>
+                  <NavLink to="/thanhtoan" className='btn d-flex align-items-center justify-content-center' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#905700', fontSize: '22px', fontWeight: 'bold', align: 'middle' }}>
+                    Đặt phòng
+                  </NavLink>
                 </div>
-                            </form>
-                        </div>
-                    </div>
+              </form>
+            </div>
           </div>
-          </section>
-          <section>
-          <div className="slide-container" style={{ width: '100%', height: '100%' }}>
+        </div>
+      </section>
+      <section>
+        <div className="slide-container" style={{ width: '100%', height: '100%' }}>
           <header className="pt-4 pb-4" style={{ backgroundColor: "#905700", color: "#FFF", fontSize: '24px' }}>
-          <h3 align="center">Các hình ảnh về phòng</h3>
+            <h3 align="center">Các hình ảnh về phòng</h3>
           </header>
           <Slide >
             {collection.map((image, index) => (
-              <div  className="each-slide" key={index}>
-                <div style={{height: '900px', width: '100%', 'backgroundImage': `url(${image.src})`}}>
-                  <span >Slide {index + 1}</span>
+              <div className="each-slide" key={index}>
+                <div style={{ height: '900px', width: '100%', 'backgroundImage': `url(${image.src})` }}>
+                  <span >Ảnh {index + 1}</span>
                 </div>
               </div>
             ))}
           </Slide>
-          </div>
-          </section>
-          <section>
-            <div className="slide-container" style={{ width: '100%', height: '100%' }}>
-            <header className="pt-4 pb-4" style={{ backgroundColor: "#905700", color: "#FFF", fontSize: '24px' }}>
+        </div>
+      </section>
+      <section>
+        <div className="slide-container" style={{ width: '100%', height: '100%' }}>
+          <header className="pt-4 pb-4" style={{ backgroundColor: "#905700", color: "#FFF", fontSize: '24px' }}>
             <h3 align="center">Các đánh giá về phòng</h3>
-            </header>
-            </div>
-          </section>
-          <Footer style={{ marginTop: "80px" }} />
-                
-        </div >
-            );
-          };
-    export default RoomDetail;
+          </header>
+        </div>
+      </section>
+      <Footer style={{ marginTop: "80px" }} />
+
+    </div >
+  );
+};
+export default RoomDetail;
--- src/views/RoomsPage.js
+++ src/views/RoomsPage.js
@@ -4,7 +4,7 @@ import TopNav from '../components/TopNav'
 import Footer from '../components/Footer';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 const RoomsPage = ({ rooms }) => {
-        return (
+    return (
         <div>
             <TopNav />
             <header className="pt-4 pb-4" style={{ backgroundColor: "#905700", color: "#FFF" }}><h3 align="center">Loại phòng của chúng tôi</h3></header>
@@ -14,32 +14,32 @@ const RoomsPage = ({ rooms }) => {
                         <img alt="" src="images/gdcaocap2.png" style={{ width: "90%" }} />
                     </div>
                     <div className="col-md-7 align-self-center mt-2">
-                        <p style={{ fontSize:'28px'}}>Chúng tôi tự hào giới thiệu các hạng phòng đa dạng và tiện nghi, đảm bảo đáp ứng mọi nhu cầu của quý khách và mang lại trải nghiệm lưu trú đích thực. <br/> Dù bạn đang tìm kiếm một kỳ nghỉ sang trọng, một cuộc họp kinh doanh hiệu quả hay một chuyến du lịch gia đình đầy niềm vui, chúng tôi luôn sẵn lòng phục vụ.</p>
+                        <p style={{ fontSize: '28px' }}>Chúng tôi tự hào giới thiệu các hạng phòng đa dạng và tiện nghi, đảm bảo đáp ứng mọi nhu cầu của quý khách và mang lại trải nghiệm lưu trú đích thực. <br /> Dù bạn đang tìm kiếm một kỳ nghỉ sang trọng, một cuộc họp kinh doanh hiệu quả hay một chuyến du lịch gia đình đầy niềm vui, chúng tôi luôn sẵn lòng phục vụ.</p>
                     </div>
                 </div>
             </section>
             <section className="">
-                <div style={{ backgroundColor: "#905700", padding: '50px', color: '#fff'}}>
-                    <div style={{ backgroundColor: "#905700", padding: '20px'}}>
+                <div style={{ backgroundColor: "#905700", padding: '50px', color: '#fff' }}>
+                    <div style={{ backgroundColor: "#905700", padding: '20px' }}>
                         <h1> Đặt phòng ngay </h1>
                     </div>
                     <div className='form'>
                         <form className='row'>
-                        <div className="col-2 mb-3">
-                    <       input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff'}} type='date' placeholder='Ngày check in'/>
-                        </div>
-                        <div className="col-2 mb-3">
-                            <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff'}} type='date' placeholder='Ngày check out'/>
-                        </div>
-                        <div className="col-2 mb-3">
-                            <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff'}} type='number' placeholder='Số lượng người lớn'/>
-                        </div>
-                        <div className="col-2 mb-3">
-                            <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff'}} type='number' placeholder='Số lượng trẻ em'/>
-                        </div>
-                        <div className="col-3 mb-3">
-                            <input className='form-control' style={{fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff', fontSize: '22px', fontWeight: 'bold'}} type='submit' value='Xem phòng'/>
-                        </div>
+                            <div className="col-2 mb-3">
+                                <       input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff' }} type='date' placeholder='Ngày check in' />
+                            </div>
+                            <div className="col-2 mb-3">
+                                <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff' }} type='date' placeholder='Ngày check out' />
+                            </div>
+                            <div className="col-2 mb-3">
+                                <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff' }} type='number' placeholder='Số lượng người lớn' />
+                            </div>
+                            <div className="col-2 mb-3">
+                                <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff' }} type='number' placeholder='Số lượng trẻ em' />
+                            </div>
+                            <div className="col-3 mb-3">
+                                <input className='form-control' style={{ fontSize: '24px', height: '90px', borderRadius: '9px', borderColor: '#fff', fontSize: '22px', fontWeight: 'bold' }} type='submit' value='Xem phòng' />
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -47,36 +47,36 @@ const RoomsPage = ({ rooms }) => {
             <section class="container mt-5 mb-5">
                 <div class="row" >
                     {rooms.map(room => (
-                        <div className="row " style= {{backgroundColor:'#fff', alignItems: 'center', display: 'flex', borderRadius: '5px', borderStyle: 'groove', marginTop: '50px' }}>
+                        <div className="row " style={{ backgroundColor: '#fff', alignItems: 'center', display: 'flex', borderRadius: '5px', borderStyle: 'groove', marginTop: '50px' }}>
                             <div className="col-5 outset">
                                 <NavLink to={`/rooms/${room.id}`}>
                                     <img src={room.image} alt={room.name} style={{ padding: '10px', width: "100%" }} />
                                 </NavLink>
                             </div>
                             <div className="col-5 column">
-                                <h2 className='row' style={{color: '#905700' }}>{room.name}</h2>
+                                <h2 className='row' style={{ color: '#905700' }}>{room.name}</h2>
                                 <p className='row'>
-                                    
+
                                     <h3 className='row' >Giá ưu đãi cho 1 đêm: {room.cost}</h3>
                                 </p>
                                 <div className='row g-2 pb-3'>
-                                            <div className='col-auto' style={{ backgroundColor: "yellow", borderRadius: "5px" }}>4.3</div>
-                                            <div className='col-auto' >Cực tốt</div>
-                                            <div className='col-auto spaceText' style={{ color: "gray" }}>220 đánh giá</div>
+                                    <div className='col-auto' style={{ backgroundColor: "yellow", borderRadius: "5px" }}>4.3</div>
+                                    <div className='col-auto' >Cực tốt</div>
+                                    <div className='col-auto spaceText' style={{ color: "gray" }}>220 đánh giá</div>
                                 </div>
-                            </div>  
+                            </div>
                             <div className="col-2 ">
-                                    <button type="button" style={{color: '#fff', fontWeight: 'bold', backgroundColor: '#905700', }} className='bluecolor block m-2 py-2 px-4 rounded'> Đặt phòng </button>
+                                <button type="button" style={{ color: '#fff', fontWeight: 'bold', backgroundColor: '#905700', }} className='bluecolor block m-2 py-2 px-4 rounded'> Đặt phòng </button>
                             </div>
                         </div>
-                        )
+                    )
                     )}
-                    
+
                 </div>
 
             </section>
             <Footer style={{ marginTop: "80px" }} />
-            
+
         </div >
     );
 }
